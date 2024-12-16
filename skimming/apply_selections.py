fe0281c7a8961b52d7d071b317b751c650c72078
marcellocosti <marcellodicostanzo00@gmail.com>
2024-12-16 15:36:33 +0100

    implemented selections also for .root trees

--- skimming/apply_selections.py
+++ skimming/apply_selections.py
@@ -17,19 +17,36 @@ with open(args.cfg, "r") as stream:
 
 for itree in selections_cfg['trees']:
 
-    df_basename = os.path.splitext(os.path.basename(itree["treefile"]))[0]
-    df = pd.read_parquet(itree["treefile"])
     print("---------------------------------")
+    print(f"Loading data from {itree['treefile']}...")
+    extension = os.path.splitext(itree["treefile"])[1]
+    if extension == ".root":
+        # Load the tree
+        with uproot.open(itree['treefile'], encoding="utf8") as f:
+            df_data = []
+            for folder_name, folder in f.items():
+                if itree["treename"] in folder_name:
+                    df_data.append(folder.arrays(library="pd"))
+            df = pd.concat(df_data)
+        print("Loaded data from .root file!")
+    else:
+        df = pd.read_parquet(itree["treefile"])
+        print("Loaded data from .parquet file!")
+    
+    
+    df_basename = os.path.splitext(os.path.basename(itree["treefile"]))[0]
     print(f"Selection: {itree['selections'] if itree.get('selections') else 'None'}")
-    sel_df = df.query(f"{itree['selections']}") if itree.get('selections') else df
+    print(len(df))
+    df = df.query(f"{itree['selections']}") if itree.get('selections') else df
+    print(len(df))
     print(f"Checking directory: {os.path.dirname(f"{os.path.dirname(itree['treefile'])}/skimmed/")}")
     if not os.path.exists(os.path.dirname(f"{os.path.dirname(itree['treefile'])}/skimmed/")):
         print("Not existing! Creating ...")
         os.makedirs(os.path.dirname(f"{os.path.dirname(itree['treefile'])}/skimmed/"))
     
     if itree.get('suffix'):
-        sel_df.to_parquet(f"{os.path.dirname(itree['treefile'])}/skimmed/{df_basename}_{itree['suffix']}.parquet")
+        df.to_parquet(f"{os.path.dirname(itree['treefile'])}/skimmed/{df_basename}_{itree['suffix']}.parquet")
         print(f"Saved skimmed tree to: {f"{os.path.dirname(itree['treefile'])}/skimmed/{df_basename}_{itree['suffix']}.parquet"}")
     else:
-        sel_df.to_parquet(f"{os.path.dirname(itree['treefile'])}/skimmed/{df_basename}.parquet")
+        df.to_parquet(f"{os.path.dirname(itree['treefile'])}/skimmed/{df_basename}.parquet")
         print(f"Saved skimmed tree to: {f"{os.path.dirname(itree['treefile'])}/skimmed/{df_basename}.parquet"}")
